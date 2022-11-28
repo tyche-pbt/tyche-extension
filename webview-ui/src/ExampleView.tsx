29e4f89a8e5573fca87e16fae9bc4e29cc05bbc5
Harrison Goldstein <hgoldstein95@gmail.com>
2022-11-27 23:14:02 -0500

    Add slicing to example view

--- webview-ui/src/ExampleView.tsx
+++ webview-ui/src/ExampleView.tsx
@@ -1,14 +1,24 @@
-import * as React from "react";
 import { SampleInfo } from "./datatypes";
-import { VSCodeDivider } from "@vscode/webview-ui-toolkit/react";
+import { VSCodeButton, VSCodeDivider } from "@vscode/webview-ui-toolkit/react";
 import { PrettyExample } from "./PrettyExample";
+import { useState } from "react";
 
 type ExampleViewProps = {
   dataset: SampleInfo[];
 };
 
 export const ExampleView = (props: ExampleViewProps) => {
+  const [page, setPage] = useState(0);
+
   return (<div className="ExampleView">
-    {props.dataset.slice(0, 4).flatMap(x => [<VSCodeDivider />, <PrettyExample example={x} />]).slice(1)}
+    {props.dataset.slice(page, page + 4).flatMap(x => [<VSCodeDivider />, <PrettyExample example={x} />]).slice(1)}
+    <div className="page-buttons">
+      <VSCodeButton onClick={() => setPage(page < 4 ? props.dataset.length - 4 : page - 4)}>
+        ◀
+      </VSCodeButton>
+      <VSCodeButton onClick={() => setPage(page + 4 >= props.dataset.length ? 0 : page + 4)}>
+        ▶
+      </VSCodeButton>
+    </div>
   </div>);
 };
