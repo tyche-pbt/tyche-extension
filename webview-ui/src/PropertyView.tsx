19ed4507d341dcad9e786a90d113939479bafdef
Harrison Goldstein <hgoldstein95@gmail.com>
2023-12-24 12:50:36 -0500

    Simplify example view

--- webview-ui/src/PropertyView.tsx
+++ webview-ui/src/PropertyView.tsx
@@ -25,36 +25,37 @@ const PropertyView = (props: PropertyViewProps) => {
     .reduce((acc, curr) => Array.from(new Set<string>([...acc, ...curr])), []);
 
   return <>
-    <Dialog open={exampleFilter !== undefined} onClose={() => setExampleFilter(undefined)}>
-      <Dialog.Panel className="fixed top-12 left-0 right-0 bottom-0 bg-background px-3 overflow-scroll z-10">
-        <div className="fixed top-0 right-0 left-0 bg-primary py-2 px-3 h-10 flex justify-between items-center">
+    {exampleFilter !== undefined &&
+      <>
+        <div className="fixed top-0 right-0 left-0 bg-accent py-2 px-3 h-10 flex justify-between items-center">
           <button onClick={() => setExampleFilter(undefined)}>
             <i className="codicon codicon-close text-background" />
           </button>
         </div>
         <ExampleView filter={exampleFilter} dataset={testInfo.samples} />
-      </Dialog.Panel>
-    </Dialog>
-    {exampleFilter ? <></> : <>
-      <Card>
-        <div className="text-lg font-bold">
-          Tyche Analysis
-        </div>
-        <span className="font-mono text-sm">
-          {property}
-        </span>
-      </Card>
-      <Info status={testInfo.status === "failure" ? "failure" : "success"} info={testInfo.info} />
-      {testInfo.status === "failure" &&
-        <FailingCases dataset={testInfo.samples} />
-      }
-      <HighLevelStats testInfo={testInfo} property={property} />
-      <Charts
-        setFilteredView={setExampleFilter}
-        dataset={testInfo.samples}
-        features={{ numerical, categorical }}
-      />
-    </>
+      </>
+    }
+    {exampleFilter === undefined &&
+      <>
+        <Card>
+          <div className="text-lg font-bold">
+            Tyche Analysis
+          </div>
+          <span className="font-mono text-sm">
+            {property}
+          </span>
+        </Card>
+        <Info status={testInfo.status === "failure" ? "failure" : "success"} info={testInfo.info} />
+        {testInfo.status === "failure" &&
+          <FailingCases dataset={testInfo.samples} />
+        }
+        <HighLevelStats testInfo={testInfo} property={property} />
+        <Charts
+          setFilteredView={setExampleFilter}
+          dataset={testInfo.samples}
+          features={{ numerical, categorical }}
+        />
+      </>
     }
   </>;
 };
