0a522d90b3304fd20a66790d752b3fc9775e4cde
Jeff Tao <jatao88@gmail.com>
2024-01-29 13:01:05 -0500

    Feat: Show duplicate counts, improve ExampleView re-render efficiency

--- webview-ui/src/ExampleView.tsx
+++ webview-ui/src/ExampleView.tsx
@@ -2,7 +2,7 @@ import { ExampleFilter, SampleInfo } from "./report";
 import { StaticCard } from "./ui/Card";
 import { PrettyExample } from "./ui/PrettyExample";
 import { Transition } from "@headlessui/react";
-import { useState } from "react";
+import { useState, useMemo, memo } from "react";
 import { useDebounce } from "./utilities/useDebounce";
 
 type ExampleViewProps = {
@@ -11,40 +11,58 @@ type ExampleViewProps = {
   closeExamples: () => void;
 };
 
-export const ExampleView = (props: ExampleViewProps) => {
+export const ExampleView = ({ dataset, filter, closeExamples }: ExampleViewProps) => {
   const [textSearchFilter, setTextSearchFilter] = useState("");
-  const debouncedTextSearchFilter = useDebounce(textSearchFilter, 500);
+  const debouncedTextSearchFilter = useDebounce(textSearchFilter, 300);
   const onTextFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTextSearchFilter(e.target.value);
+    setTextSearchFilter(() => e.target.value);
   };
 
-  const { filter } = props;
+  // We need to memoize this for referential equality to prevent the sample list from rerendeing
+  // when this component's state updates
+  const [processedSamples, filterText] = useMemo(() => {
+    // Apply example filter
+    let exampleFiltered: SampleInfo[];
+    let filterText: string;
+    if (filter === undefined) {
+      exampleFiltered = dataset;
+      filterText = "";
+    } else if ("ordinal" in filter) {
+      exampleFiltered = dataset.filter((x) => x.features.ordinal[filter.ordinal] === filter.value);
+      filterText = `${filter.ordinal} = ${filter.value}`;
+    } else if ("nominal" in filter) {
+      exampleFiltered = dataset.filter((x) => x.features.nominal[filter.nominal] === filter.value);
+      filterText = `${filter.nominal} = ${filter.value}`;
+    } else {
+      exampleFiltered = filter.examples;
+      filterText = filter.subset;
+    }
 
-  let dataset: SampleInfo[];
-  let filterText: string;
+    // Apply text filter
+    const textFiltered = exampleFiltered.filter((x) =>
+      x.item.toLocaleLowerCase().includes(debouncedTextSearchFilter.toLocaleLowerCase())
+    );
 
-  if (filter === undefined) {
-    dataset = props.dataset;
-    filterText = "";
-  } else if ("ordinal" in filter) {
-    dataset = props.dataset.filter((x) => x.features.ordinal[filter.ordinal] === filter.value);
-    filterText = `${filter.ordinal} = ${filter.value}`;
-  } else if ("nominal" in filter) {
-    dataset = props.dataset.filter((x) => x.features.nominal[filter.nominal] === filter.value);
-    filterText = `${filter.nominal} = ${filter.value}`;
-  } else {
-    dataset = filter.examples;
-    filterText = filter.subset;
-  }
+    // Deduplicate & count
+    const deduped: { [key: string]: [SampleInfo, number] } = {};
+    textFiltered.forEach((x) => {
+      if (x.item in deduped) {
+        deduped[x.item][1] += 1;
+      } else {
+        deduped[x.item] = [x, 1];
+      }
+    });
 
-  dataset = dataset.filter((x) =>
-    x.item.toLocaleLowerCase().includes(debouncedTextSearchFilter.toLocaleLowerCase())
-  );
+    // Apply sort
+    const sortedSamples = Object.entries(deduped).sort(([, [, a]], [, [, b]]) => b - a);
+
+    return [sortedSamples, filterText];
+  }, [dataset, filter, debouncedTextSearchFilter]);
 
   return (
     <>
       <div className="fixed top-0 left-0 right-0 z-40 flex items-center justify-between h-10 px-3 py-2 bg-accent">
-        <button onClick={props.closeExamples}>
+        <button onClick={closeExamples}>
           <i className="codicon codicon-close text-background" />
         </button>
         <div>
@@ -65,18 +83,34 @@ export const ExampleView = (props: ExampleViewProps) => {
         enterFrom="opacity-0 -translate-x-8"
         enterTo="opacity-100"
         as="div">
-        <div className="w-full">
-          <StaticCard>
-            <div className="text-lg font-bold">Samples</div>
-            <span className="font-mono">{filterText}</span>
-          </StaticCard>
-          {dataset.map((x, i) => (
-            <StaticCard key={`example-${i}`} className="mt-2">
-              <PrettyExample example={x} highlightText={debouncedTextSearchFilter} />
-            </StaticCard>
-          ))}
-        </div>
+        <SampleList
+          samples={processedSamples}
+          filterText={filterText}
+          highlightText={debouncedTextSearchFilter}
+        />
       </Transition>
     </>
   );
 };
+
+type SampleListProps = {
+  filterText: string;
+  highlightText: string;
+  samples: [string, [SampleInfo, number]][];
+};
+
+// This component is memoized to prevent the list from rerendering unnecessarily when the
+// ExampleView updates, such as when typing in the text filter box
+const SampleList = memo(({ filterText, samples, highlightText }: SampleListProps) => (
+  <div className="w-full">
+    <StaticCard>
+      <div className="text-lg font-bold">Samples</div>
+      <span className="font-mono">{filterText}</span>
+    </StaticCard>
+    {samples.map(([key, [x, count]], i) => (
+      <StaticCard key={key} className="mt-2">
+        <PrettyExample example={x} highlightText={highlightText} duplicateCount={count} />
+      </StaticCard>
+    ))}
+  </div>
+));
--- webview-ui/src/ui/PrettyExample.tsx
+++ webview-ui/src/ui/PrettyExample.tsx
@@ -4,35 +4,45 @@ import { SampleInfo } from "../report";
 type PrettyExampleProps = {
   example: SampleInfo;
   highlightText?: string;
+  duplicateCount?: number;
 };
 
 export const PrettyExample = (props: PrettyExampleProps) => {
   return (
-    <div className="w-full">
-      <Disclosure>
-        {({ open }) => (
-          <>
-            <Disclosure.Button
-              as="pre"
-              className={`text-sm w-full break-words whitespace-pre-wrap ${
-                open ? "cursor-zoom-out" : "cursor-zoom-in"
-              }`}>
-              {props.example.item !== "" ? (
-                <PrettyText text={props.example.item} highlightText={props.highlightText} />
-              ) : (
-                <span className="font-sans italic">(no representation)</span>
-              )}
-            </Disclosure.Button>
-            <Disclosure.Panel
-              as="pre"
-              className="w-full mt-2 text-sm break-words whitespace-pre-wrap">
-              <div className="font-sans">Details:</div>
-              {JSON.stringify(props.example.dataLine, null, 2)}
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    </div>
+    <>
+      <div className="w-full">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button
+                as="div"
+                className={`flex flex-row ${open ? "cursor-zoom-out" : "cursor-zoom-in"}`}>
+                <pre className="w-full text-sm break-words whitespace-pre-wrap">
+                  {props.example.item !== "" ? (
+                    <PrettyText text={props.example.item} highlightText={props.highlightText} />
+                  ) : (
+                    <span className="font-sans italic">(no representation)</span>
+                  )}
+                </pre>
+                <div className="flex flex-col justify-center">
+                  {props.duplicateCount !== undefined && props.duplicateCount > 1 && (
+                    <div className="flex items-center justify-center w-10 p-1 text-white rounded-full aspect-square bg-accent text-s">
+                      {props.duplicateCount}&times;
+                    </div>
+                  )}
+                </div>
+              </Disclosure.Button>
+              <Disclosure.Panel
+                as="pre"
+                className="w-full mt-2 text-sm break-words whitespace-pre-wrap">
+                <div className="font-sans">Details:</div>
+                {JSON.stringify(props.example.dataLine, null, 2)}
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </>
   );
 };
 
@@ -43,8 +53,10 @@ type PrettyTextProps = {
 
 const PrettyText = (props: PrettyTextProps) => {
   const sections =
-    props.highlightText != undefined && props.highlightText.length > 0
-      ? props.text.split(new RegExp(`(${props.highlightText})`, "gi"))
+    props.highlightText !== undefined && props.highlightText.length > 0
+      ? props.text.split(
+          new RegExp(`(${props.highlightText.replace(/[/\-\\^$*+?.()|[\]{}]/g, "\\$&")})`, "gi")
+        )
       : [props.text];
   return (
     <>
